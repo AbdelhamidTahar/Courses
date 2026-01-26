28e1e0e58c59e572c524dc2ab85286fddf5af55b
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-01-26 13:50:38 +0100

    Pointer

---
+++ C6/Lesson #35 - What is Pointer/Lesson #35 - What is Pointer/Lesson #35 - What is Pointer.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+	int V1 = 100;
+	cout <<"Value Of int V1 is:" << V1 << endl;
+	cout << "Value Of add int V1 is:" << &V1 << endl;
+
+	int* P1 = &V1;
+	cout << "Value Of int P1 is:" << P1 << endl;
+	cout << "Value Of add int P1 is:" << &P1 << endl;
+
+
+
+
+
+
+	return 0;
+}
