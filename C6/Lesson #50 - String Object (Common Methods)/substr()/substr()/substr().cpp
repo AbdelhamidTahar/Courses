b8b040c0e429c3f6dbe396f5b2265c52837dc129
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-02-02 11:37:33 +0100

    substr(index,how many char after index ) method use the one  paremetar as how index and use The second parametar as how many char do you want afhter index in parametar one

---
+++ C6/Lesson #50 - String Object (Common Methods)/substr()/substr()/substr().cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string S1 = "Abdelhamid Tahar";
+
+
+
+    // insert Method use sting as prametar only no char.
+    cout<< S1.substr(3, 4)<< endl;
+    
+
+    cout << S1;
+}
