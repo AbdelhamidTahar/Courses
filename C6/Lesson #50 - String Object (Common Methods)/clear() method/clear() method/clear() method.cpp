0edc03a3f4d6a5c3bcc22e91503f7136c8ed52b1
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-02-02 12:07:40 +0100

    using .clear() method to remove all char in string

---
+++ C6/Lesson #50 - String Object (Common Methods)/clear() method/clear() method/clear() method.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string S1 = "Abdelhamid Tahar";
+
+
+
+    // clear all char in string.
+    S1.clear();
+    cout << S1 << endl;
+
+    return 0;
+
+}
