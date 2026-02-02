c011220bb9f52322bd9fa7a18c4580ccb0ebc857
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-02-02 11:19:52 +0100

    edit syntax

--- C6/Lesson #50 - String Object (Common Methods)/S1.at()/S1.at()/S1.at().cpp
+++ C6/Lesson #50 - String Object (Common Methods)/S1.at()/S1.at()/S1.at().cpp
@@ -7,12 +7,14 @@ int main()
     string S1 = "Abdelhamid Tahar";
 
     for (int i =0 ; i <= S1.length();i++)
-   try
-   {
-       cout << S1.at(i) << endl;
-   }
-   catch (...)
-   {
-       cout << "Not Found";
-   }
+    {
+        try
+        {
+            cout << S1.at(i) << endl;
+        }
+        catch (...)
+        {
+            cout << "Not Found";
+        }
+    }
 }
