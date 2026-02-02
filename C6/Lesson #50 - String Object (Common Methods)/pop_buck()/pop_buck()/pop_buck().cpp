4200477cd7ff7eee1727dada6ccdb99b9f170d4c
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-02-02 11:49:47 +0100

    pop_back method delet last char in string

---
+++ C6/Lesson #50 - String Object (Common Methods)/pop_buck()/pop_buck()/pop_buck().cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string S1 = "Abdelhamid Tahar";
+
+    
+    while (S1.length() !=0)
+    {
+
+        // pop_back Method is a void no func no parametar no gust call.
+        S1.pop_back();
+        
+    }
+
+    cout << S1;
+
+}
