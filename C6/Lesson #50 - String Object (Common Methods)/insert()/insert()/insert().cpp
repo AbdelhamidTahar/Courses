6cb69b5540682fff6d00233dddbccd18c14853fd
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-02-02 11:32:46 +0100

    insert method to add any string and any position

---
+++ C6/Lesson #50 - String Object (Common Methods)/insert()/insert()/insert().cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string S1 = "Abdelhamid Tahar";
+
+    
+   
+    // insert Method use sting as prametar only no char.
+    S1.insert(3, "Z");
+    cout << S1;
+    
+    cout << S1;
+}
