957075db7fa720a5114e6ceb5e76f58999185203
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-02-03 14:12:28 +0100

    practis whit Read Mood in files

---
+++ C6/Lesson #54 - Read Mode Print File Content/Lesson #54 - Read Mode Print File Content/Lesson #54 - Read Mode Print File Content.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+void FillFileConntent(const string &FileName)
+{
+    fstream File;
+
+    File.open(FileName+".text", ios::out | ios::app);
+        if (File.is_open())
+        {
+            for(int i = 0 ; i<100;i++)
+            {
+                File << "Hello\n";
+            }
+        }
+        File.close();
+}
+void ReadFileConntent(const string& FileName)
+{
+
+  
+    fstream File;
+
+    File.open(FileName + ".text", ios::in);
+
+    if (File.is_open())
+    {
+        string Line = "";
+        while (getline(File, Line))
+        {
+            cout << Line << endl;
+        }
+
+    }
+    File.close();
+   
+}
+
+int main()
+{
+
+    string FileName = "TestFile";
+    FillFileConntent(FileName);
+    ReadFileConntent(FileName);
+}
