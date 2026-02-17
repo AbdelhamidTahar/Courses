94d3dca9f4f36e1d4a284b4f10ca7624c98c2bce
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-02-17 14:07:04 +0100

    Sum cols matrix and save sum only col in array element and print array

---
+++ C7/P5 Sun matrix Columns ans save in array/P5 Sun matrix Columns ans save in array/P5 Sun matrix Columns ans save in array.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <iomanip>
+#include "GenerateLib.h"
+using namespace std;
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short j = 0; j < Cols; j++)
+		{
+			arr[i][j] = Generates::RandomNumber(1, 100);
+		}
+	}
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short j = 0; j < Cols; j++)
+		{
+			cout << setw(3) << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+int ColSum(int arr[3][3], short Rows, short ColNumber)
+{
+	int Sum = 0;
+	for (short i = 0; i < Rows; i++)
+	{
+		Sum += arr[i][ColNumber];
+	}
+
+	return Sum;
+}
+
+void SumMatixColsInArry(int arr[3][3], int arrSum[3], short Rows,short Cols)
+{
+	for (short i = 0; i < Cols; i++)
+	{
+		arrSum[i] = ColSum(arr, Rows, i);
+	}
+}
+
+void PrintEachColSum(int arrSum[3], short Length)
+{
+	cout << "\nThe Following Are The Sum Of Each Col In The Matrix: \n";
+
+	for (short j = 0; j < Length; j++)
+	{
+		cout << "Col " << j + 1 << " Sum: " << arrSum[j] << "\n";
+	}
+	cout << endl;
+}
+
+int main()
+{
+
+	srand((unsigned)time(NULL));
+	int arr[3][3];
+	int arrSum[3];
+	FillMatrixWithRandomNumbers(arr, 3, 3);
+	cout << "\nThe following is a 3x3 random matrix:\n";
+	PrintMatrix(arr, 3, 3);
+	SumMatixColsInArry(arr, arrSum, 3, 3);
+	PrintEachColSum(arrSum, 3);
+
+	return 0;
+}
