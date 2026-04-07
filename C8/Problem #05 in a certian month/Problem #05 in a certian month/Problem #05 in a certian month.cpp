fa07b50d43ccd3f5aba8532c90d70170751e8888
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-04-07 15:42:47 +0100

    My teache rcode

--- C8/Problem #05 in a certian month/Problem #05 in a certian month/Problem #05 in a certian month.cpp
+++ C8/Problem #05 in a certian month/Problem #05 in a certian month/Problem #05 in a certian month.cpp
@@ -17,50 +17,62 @@ short ReadMonth()
 	return Month;
 }
 
-bool IsLeapYear(short Year)
+bool isLeapYear(short Year)
 {
 	return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
 }
 
-short NumberOfDaysInMonth (short Year,short Month)
+short NumberOfDaysInMonth (short Month,short Year)
 {
-	short DaysInMonth[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
-	DaysInMonth[1] = IsLeapYear(Year)? 29 : 28;
-	return DaysInMonth[Month - 1];
+	if (Month < 1 || Month>12)
+		return 0;
+	if (Month == 2)
+	{
+		return isLeapYear(Year) ? 29 : 28;
+	}
+	short arr31Days[7] = { 1,3,5,7,8,10,12 };
+	for (short i = 1; i <= 7; i++)
+	{
+		if (arr31Days[i - 1] == Month)
+			return 31;
+	}
+	//if you reach here then its 30 days.
+	return 30;
+
 }
 
-short NumberOfHoursInMonth(short Year,short Month)
+short NumberOfHoursInMonth(short Month,short Year)
 {
 
-	return NumberOfDaysInMonth(Year, Month) * 24;
+	return NumberOfDaysInMonth(Month, Year) * 24;
 }
 
-int NumberOfMinutesInMonth(short Year,short Month)
+int NumberOfMinutesInMonth(short Month,short Year)
 {
 
-	return NumberOfHoursInMonth(Year, Month) * 60;
+	return NumberOfHoursInMonth(Month, Year) * 60;
 }
 
-int NumberOfSecondsInMonth(short Year,short Month)
+int NumberOfSecondsInMonth(short Month,short Year)
 {
 
-	return NumberOfMinutesInMonth(Year, Month) * 60;
+	return NumberOfMinutesInMonth(Month, Year) * 60;
 }
 
-void PrintCertainMonth(short Year,short Month)
+void PrintCertainMonth    (short Month,short Year)
 {
 	cout << "\n\n";
-	cout << "Number Of Days    in Year [" << Month << "] is " << NumberOfDaysInMonth   (Year,Month) << endl;
-	cout << "Number Of Hours   in Year [" << Month << "] is " << NumberOfHoursInMonth  (Year,Month) << endl;
-	cout << "Number Of Minutes in Year [" << Month << "] is " << NumberOfMinutesInMonth(Year,Month) << endl;
-	cout << "Number Of Seconds in Year [" << Month << "] is " << NumberOfSecondsInMonth(Year,Month) << endl;
+	cout << "Number Of Days    in Year [" << Month << "] is " << NumberOfDaysInMonth   (Month,Year) << endl;
+	cout << "Number Of Hours   in Year [" << Month << "] is " << NumberOfHoursInMonth  (Month,Year) << endl;
+	cout << "Number Of Minutes in Year [" << Month << "] is " << NumberOfMinutesInMonth(Month,Year) << endl;
+	cout << "Number Of Seconds in Year [" << Month << "] is " << NumberOfSecondsInMonth(Month,Year) << endl;
 }
 
 int main()
 {
 	short Year = ReadYear();
 	short Month = ReadMonth();
-	PrintCertainMonth(Year,Month);
+	PrintCertainMonth(Month,Year);
 	system("pause>0");
 
 
