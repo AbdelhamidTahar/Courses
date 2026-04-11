411d33c09fae4213cddfca842cbd0d0101321fa4
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-04-11 13:42:14 +0100

    My Code Month Calendar

--- C8/Problem #07 Find Day Birth Day/Problem #07 Find Day Birth Day/Problem #07 Find Day Birth Day.cpp
+++ C8/Problem #07 Find Day Birth Day/Problem #07 Find Day Birth Day/Problem #07 Find Day Birth Day.cpp
@@ -8,8 +8,7 @@ short DayOfWeekOrder(short Day, short Month, short Year)
 	m = Month + (12 * a) - 2;
 	// Gregorian:
 	//0:sun, 1:Mon, 2:Tue...etc
-	return (Day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m)
-		/ 12)) % 7;
+	return (Day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m)/ 12)) % 7;
 }
 
 string DayShortName(short DayOfWeekOrder)
