eb2d5f6ce6cc043dfe33a3ba9c5d43af585e1ac7
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-04-18 13:19:45 +0100

    My Code

---
+++ C8/Problem #15 Is Last Day,Is Last Month/Problem #15 Is Last Day,Is Last Month/Problem #15 Is Last Day,Is Last Month.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isLeapYear(short Year)
+{
+	// if year is divisible by 4 AND not divisible by 100
+	// OR if year is divisible by 400
+	// then it is a leap year
+	return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+}
+
+short NumberOfDaysInAMonth(short Month, short Year)
+{
+	if (Month < 1 || Month>12)
+		return 0;
+	int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	return (Month == 2) ? (isLeapYear(Year) ? 29 : 28) : days[Month - 1];
+}
+
+short ReadYear()
+{
+	short Year;
+	cout << "\nPlease enter a year? ";
+	cin >> Year;
+	return Year;
+}
+
+short ReadMonth()
+{
+	short Month;
+	cout << "\nPlease enter a Month? ";
+	cin >> Month;
+	return Month;
+}
+
+short ReadDay()
+{
+	short Day;
+	cout << "\nPlease enter a Day? ";
+	cin >> Day;
+	return Day;
+}
+
+struct stDate
+{
+	short Year;
+	short Month;
+	short Day;
+};
+
+stDate ReadFullDate()
+{
+	stDate Date;
+	Date.Day = ReadDay();
+	Date.Month = ReadMonth();
+	Date.Year = ReadYear();
+	return Date;
+}
+
+bool IsDayLastdayInMonth(stDate Date)
+{
+	return Date.Day == NumberOfDaysInAMonth(Date.Month, Date.Year);
+}
+
+bool IsMonthLastMonthInYear(short Month)
+{
+	return Month == 12;
+}
+
+
+int main()
+{
+
+	stDate Date1;
+	Date1 = ReadFullDate();
+
+	if (IsDayLastdayInMonth(Date1))
+		cout << "\nYes, Day Is Last Day In Month\n";
+	else
+		cout << "\nNo, Day Is  Not Last Day In Month\n";
+
+
+	if (IsMonthLastMonthInYear(Date1.Month))
+		cout << "\nYes, Month Is Last Month In Year\n";
+	else
+		cout << "\nNo, Month Is  Not Last Month In Year\n";
+
+
+	return 0;
+}
