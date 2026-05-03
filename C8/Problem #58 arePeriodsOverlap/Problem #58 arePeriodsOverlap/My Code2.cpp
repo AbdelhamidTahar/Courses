586793ddbbf5213d06d9188e650ab9809753cde9
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-05-03 14:29:06 +0100

    My Code

---
+++ C8/Problem #58 arePeriodsOverlap/Problem #58 arePeriodsOverlap/My Code2.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+using namespace std;
+
+struct stDate
+{
+	short Year;
+	short Month;
+	short Day;
+};
+
+struct stPeriod
+{
+	stDate StartDate;
+	stDate EndDate;
+};
+
+
+
+short ReadDay()
+{
+	short Day;
+	cout << "\nPlease enter a Day? ";
+	cin >> Day;
+	return Day;
+}
+
+short ReadMonth()
+{
+	short Month;
+	cout << "\nPlease enter a Month? ";
+	cin >> Month;
+	return Month;
+}
+
+short ReadYear()
+{
+	short Year;
+	cout << "\nPlease enter a Year? ";
+	cin >> Year;
+	return Year;
+}
+
+stDate ReadFullDate()
+{
+	stDate Date;
+	Date.Day = ReadDay();
+	Date.Month = ReadMonth();
+	Date.Year = ReadYear();
+	return Date;
+}
+
+stPeriod ReadFullPeriod()
+{
+	stPeriod Period;
+
+	cout << "Enter Start Date:\n\n";
+	Period.StartDate = ReadFullDate();
+
+	cout << "\n\nEnter End Date:\n\n";
+	Period.EndDate = ReadFullDate();
+
+	return Period;
+}
+
+bool IsDate1BeforeDate2(stDate Date1, stDate Date2)
+{
+	return (Date1.Year < Date2.Year) ? true : ((Date1.Year ==
+		Date2.Year) ? (Date1.Month < Date2.Month ? true : (Date1.Month ==
+			Date2.Month ? Date1.Day < Date2.Day : false)) : false);
+}
+
+bool IsDate1EqualDate2(stDate Date1, stDate Date2)
+{
+	return (Date1.Year == Date2.Year) ? ((Date1.Month ==
+		Date2.Month) ? ((Date1.Day == Date2.Day) ? true : false) : false)
+		: false;
+}
+
+bool IsDate1AfterDate2(stDate Date1, stDate Date2)
+{
+	return (!IsDate1BeforeDate2(Date1, Date2) && !IsDate1EqualDate2(Date1, Date2));
+}
+
+
+bool isLeapYear(short Year)
+{
+	// if year is divisible by 4 AND not divisible by 100
+	// OR if year is divisible by 400
+	// then it is a leap year
+	return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+}
+
+short NumberOfDaysInAMonth(short Month, short Year)
+{
+	if (Month < 1 || Month>12)
+		return 0;
+	int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	return (Month == 2) ? (isLeapYear(Year) ? 29 : 28) : days[Month - 1];
+}
+
+bool IsLastDayInMonth(stDate Date)
+{
+	return Date.Day == NumberOfDaysInAMonth(Date.Month, Date.Year);
+}
+
+bool IsLastMonthInYear(short Month)
+{
+	return Month == 12;
+}
+
+stDate IncreaseDateByOneDay(stDate Date)
+{
+	if (IsLastDayInMonth(Date))
+	{
+		if (IsLastMonthInYear(Date.Month))
+		{
+			Date.Month = 1;
+			Date.Day = 1;
+			Date.Year++;
+		}
+		else
+		{
+			Date.Day = 1;
+			Date.Month++;
+		}
+	}
+	else
+	{
+		Date.Day++;
+	}
+	return Date;
+}
+
+
+bool IsPeriodsOverlap(stPeriod Period1, stPeriod Period2)
+{
+	while (!IsDate1AfterDate2(Period1.StartDate, Period1.EndDate))
+	{
+		if (IsDate1EqualDate2(Period1.StartDate, Period2.StartDate))
+			return true;
+		Period1.StartDate = IncreaseDateByOneDay(Period1.StartDate);
+	}
+	return false;
+}
+
+int main()
+{
+
+	stPeriod Period1, Period2;
+
+	cout << "\Enter Period 1: \n";
+	Period1 = ReadFullPeriod();
+	cout << "\nEnter Period 2: \n";
+	Period2 = ReadFullPeriod();
+
+	if (IsPeriodsOverlap(Period1, Period2))
+		cout << "\nYes Periods Overlap\n";
+	else
+		cout << "\nNo Periods are Not Overlap\n";
+
+	return 0;
+}
