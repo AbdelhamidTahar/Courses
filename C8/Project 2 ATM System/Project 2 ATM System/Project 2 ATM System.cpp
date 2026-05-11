f0fe32a80a50bf4a8df1084c712b7b2b8c74ba79
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-05-11 18:41:12 +0100

    fix bugs

--- C8/Project 2 ATM System/Project 2 ATM System/Project 2 ATM System.cpp
+++ C8/Project 2 ATM System/Project 2 ATM System/Project 2 ATM System.cpp
@@ -8,15 +8,18 @@ using namespace std;
 
 const string ClientsFileName = "Clients.txt";
 
+void ShowQuickWithdrawMenuSecreen();
+void ShowNormalWithdrawSecreen();
+void Login();
 
 enum enATMMenueOptions {
-    eQuickWithdraw = 1, eNormlWithdraw = 2,
-    eDeposit = 3, eChangrBalance = 4, eLogout = 5
+    eQuickWithdraw = 1, eNormalWithdraw = 2,
+    eDeposit = 3, eCheckrBalance = 4, eLogout = 5
 };
 
 enum enQuickWithdrawOptions {
-    ewp1 = 20, ewp2 = 50, ewp3 = 1000, ewp4 = 200,
-    ewp5 = 400, ewp6 = 600, ewp7 = 800, ewp8 = 1000, ewpExit = 9
+    ewp1 = 1, ewp2 = 2, ewp3 = 3, ewp4 = 4,
+    ewp5 = 5, ewp6 = 6, ewp7 = 7, ewp8 = 8, ewpExit = 9
 };
 
 struct sClient
@@ -140,7 +143,7 @@ bool LoadClientInfo(string AccountNumber, string PinCode)
         return false;
 }
 
-short ReadMainMenueOption()
+short ReadMainATMMenueOption()
 {
     cout << "Choose what do you want to do? [1 to 5]? ";
     short Choice = 0;
@@ -167,7 +170,7 @@ short ReadMQuickWithdrawMenuSecreenOption()
 }
 
 
-bool IsBalanceEnough(sClient CLient, short Balance)
+bool IsBalanceEnough(sClient CLient, double Balance)
 {
     return (CLient.AccountBalance >= Balance);
 }
@@ -179,11 +182,157 @@ void GoBackShowQuickWithdrawMenuSecreen()
     ShowQuickWithdrawMenuSecreen();
 }
 
+string ConvertRecordToLine(sClient Client, string Seperator = "#//#")
+{
+
+    string stClientRecord = "";
+
+    stClientRecord += Client.AccountNumber + Seperator;
+    stClientRecord += Client.PinCode + Seperator;
+    stClientRecord += Client.Name + Seperator;
+    stClientRecord += Client.Phone + Seperator;
+    stClientRecord += to_string(Client.AccountBalance);
+
+    return stClientRecord;
+
+}
+
+
+vector <sClient> SaveCleintsDataToFile(string FileName, vector <sClient> vClients)
+{
+
+    fstream MyFile;
+    MyFile.open(FileName, ios::out);//overwrite
+
+    string DataLine;
+
+    if (MyFile.is_open())
+    {
+
+        for (sClient C : vClients)
+        {
+
+            if (C.MarkForDelete == false)
+            {
+                //we only write records that are not marked for delete.  
+                DataLine = ConvertRecordToLine(C);
+                MyFile << DataLine << endl;
+
+            }
+
+        }
+
+        MyFile.close();
+
+    }
+
+    return vClients;
+
+}
+
+
+bool DepositBalanceToClientByAccountNumberATM(string AccountNumber, double Amount, vector <sClient>& vClients)
+{
+    char Answer = 'n';
+    cout << "\n\nAre you sure you want perfrom this transaction? y/n ? ";
+    cin >> Answer;
+    if (Answer == 'y' || Answer == 'Y')
+    {
+
+        for (sClient& C : vClients)
+        {
+            if (C.AccountNumber == AccountNumber)
+            {
+                C.AccountBalance += Amount;
+                CurrentClient = C;
+                SaveCleintsDataToFile(ClientsFileName, vClients);
+                cout << "\n\nDone Successfully. New balance is: " << C.AccountBalance;
+
+                return true;
+            }
+
+        }
+
+
+        return false;
+    }
+
+}
+
+void DepositQuickWithdrawBalanceOptions(string AccountNumber, double Amount)
+{
+    if (IsBalanceEnough(CurrentClient, Amount))
+    {
+        vector<sClient>vClients = LoadCleintsDataFromFile(ClientsFileName);
+        DepositBalanceToClientByAccountNumberATM(CurrentClient.AccountNumber, Amount * -1, vClients);
+    }
+    else
+    {
+        cout << "The amount exceeds your balance, make another choice.";
+
+    }
+}
+
+void PerfromQuickWithdrawMenueOption(enQuickWithdrawOptions QuickWithdrawOptions)
+{
+    switch (QuickWithdrawOptions)
+    {
+
+    case enQuickWithdrawOptions::ewp1:
+    {
+        DepositQuickWithdrawBalanceOptions(CurrentClient.AccountNumber, 20);
+        GoBackToATMMainMenue();
+    }
+    case enQuickWithdrawOptions::ewp2:
+    {
+        DepositQuickWithdrawBalanceOptions(CurrentClient.AccountNumber,50);
+        GoBackToATMMainMenue();
+    }
+    case enQuickWithdrawOptions::ewp3:
+    {
+        DepositQuickWithdrawBalanceOptions(CurrentClient.AccountNumber, 100);
+        GoBackToATMMainMenue();
+    }
+    case enQuickWithdrawOptions::ewp4:
+    {
+        DepositQuickWithdrawBalanceOptions(CurrentClient.AccountNumber, 200);
+        GoBackToATMMainMenue();
+    }
+    case enQuickWithdrawOptions::ewp5:
+    {
+        DepositQuickWithdrawBalanceOptions(CurrentClient.AccountNumber, 400);
+        GoBackToATMMainMenue();
+    }
+    case enQuickWithdrawOptions::ewp6:
+    {
+        DepositQuickWithdrawBalanceOptions(CurrentClient.AccountNumber, 600);
+        GoBackToATMMainMenue();
+    }
+    case enQuickWithdrawOptions::ewp7:
+    {
+        DepositQuickWithdrawBalanceOptions(CurrentClient.AccountNumber, 800);
+        GoBackToATMMainMenue();
+    }
+    case enQuickWithdrawOptions::ewp8:
+    {
+        DepositQuickWithdrawBalanceOptions(CurrentClient.AccountNumber, 1000);
+        GoBackToATMMainMenue();
+    }
+        
+    case enQuickWithdrawOptions::ewpExit:
+    {
+        GoBackToATMMainMenue();
+        break;
+    }
+    
+    }
+}
+
 void ShowQuickWithdrawMenuSecreen()
 {
     system("cls");
     cout << "===========================================\n";
-    cout << "\t\tATM Main Menue Screen\n";
+    cout << "\t\tATM Quick With draw Menue Screen\n";
     cout << "===========================================\n";
     cout << "\t[1] 20          [2] 50\n";
     cout << "\t[3] 100         [4] 200\n";
@@ -193,35 +342,170 @@ void ShowQuickWithdrawMenuSecreen()
     cout << "===========================================\n";
     cout << "Your Balance is " << CurrentClient.AccountBalance;
     
-    short QuickWithdrhoice = ReadMQuickWithdrawMenuSecreenOption();
-    if (QuickWithdrhoice > 0 && QuickWithdrhoice < 9)
-    {
-        if (IsBalanceEnough(CurrentClient, (enQuickWithdrawOptions)QuickWithdrhoice))
-        {
-
-        }
-        else
-        {
-            cout << "The amount exceeds your balance, make another choice.\n";
-            GoBackShowQuickWithdrawMenuSecreen();
-        }
-    }
-    else
-    {
-        GoBackToATMMainMenue();
-    }
+    PerfromQuickWithdrawMenueOption((enQuickWithdrawOptions)ReadMQuickWithdrawMenuSecreenOption());
+ 
 
 }
 
-void PerfromTranactionsMenueOption(enATMMenueOptions ATMMenueOptions)
+
+
+void GoBackShowNormalWithdrawSecreen()
+{
+
+    cout << "\n\nPress any key to Countenu...";
+    system("pause>0");
+    system("cls");
+    ShowNormalWithdrawSecreen();
+}
+
+double ReadAmount(string Message)
+{
+    double Amount = 0;
+    do
+    {
+        cout << Message;
+        cin >> Amount;
+    } while (Amount <= 0);
+
+        return Amount;
+}
+
+bool IsHasEnoughBalance(string AcountNumber, double Amount)
+{
+    vector<sClient>vclients = LoadCleintsDataFromFile(ClientsFileName);
+    for (sClient Client : vclients)
+    {
+        if (Client.AccountNumber == AcountNumber)
+        {
+            return (Client.AccountBalance >= Amount);
+        }
+    }
+    return false;
+}
+
+bool IsNumberMultipleOfX(int Amount, int X)
+{
+    double Reminder;
+    Reminder = Amount % X;
+    return (Reminder == 0)?true:false;
+}
+
+void NormalWithdraw()
+{
+    bool HasEnoughBalance = true;
+    bool IsAmountMultipleOfX = false;
+    double Amount;
+
+    do
+    {
+        Amount = ReadAmount("\nEnter an amount multiple of 5's ? ");
+        HasEnoughBalance = IsHasEnoughBalance(CurrentClient.AccountNumber, Amount);
+
+        if (HasEnoughBalance)
+        {
+            IsAmountMultipleOfX = IsNumberMultipleOfX((int)Amount, 5);
+
+            if (IsAmountMultipleOfX == true)
+            {
+                vector<sClient>vclients = LoadCleintsDataFromFile(ClientsFileName);
+                DepositBalanceToClientByAccountNumberATM(CurrentClient.AccountNumber, Amount * -1, vclients);
+                break;
+            }
+        }
+        else
+        {
+            cout << "\n\Enter execceds your balanc, make another choice.";
+            cout << "\n\nPress any key to Countenu...";
+            system("pause>0");
+            system("cls");
+            cout << "\n-----------------------------------\n";
+            cout << "\tNormal Withdraw Screen";
+            cout << "\n-----------------------------------\n";
+        }
+
+    } while (HasEnoughBalance == false || IsAmountMultipleOfX == false);
+
+
+
+}
+
+void ShowNormalWithdrawSecreen()
+{ 
+
+    cout << "\n-----------------------------------\n";
+    cout << "\tNormal Withdraw Screen";
+    cout << "\n-----------------------------------\n";
+    NormalWithdraw();
+
+}
+
+void Deposit()
+{
+    double Amount;
+    do 
+    {
+        cout << "Enter a positive Deposit Amount? ";
+        cin >> Amount;
+
+    } while (Amount < 0);
+    vector <sClient> vClients = LoadCleintsDataFromFile(ClientsFileName);
+    DepositBalanceToClientByAccountNumberATM(CurrentClient.AccountNumber, Amount, vClients);
+
+}
+
+void ShowDepositSecreen()
+{
+    cout << "\n-----------------------------------\n";
+    cout << "\tDeposit Screen";
+    cout << "\n-----------------------------------\n";
+    Deposit();
+}
+
+
+void CheckBalanceSecreen()
+{
+    cout << "\n-----------------------------------\n";
+    cout << "\tCheck Balance Screen";
+    cout << "\n-----------------------------------\n";
+
+    cout << "Your Balance is: "<< CurrentClient.AccountBalance;
+}
+
+
+void PerfromMainATMMenueOption(enATMMenueOptions ATMMenueOptions)
 {
     switch (ATMMenueOptions)
     {
     case enATMMenueOptions::eQuickWithdraw:
     {
+        system("cls");
         ShowQuickWithdrawMenuSecreen();
         GoBackToATMMainMenue();
+        break;
     }
+    case enATMMenueOptions::eNormalWithdraw:
+    {
+        system("cls");
+        ShowNormalWithdrawSecreen();
+        GoBackToATMMainMenue();
+        break;
+    }
+    case enATMMenueOptions::eDeposit:
+    {
+        system("cls");
+        ShowDepositSecreen();
+        GoBackToATMMainMenue();
+
+    }
+    case enATMMenueOptions::eCheckrBalance:
+    {
+        system("cls");
+        CheckBalanceSecreen();
+        GoBackToATMMainMenue();
+
+    }
+    case enATMMenueOptions::eLogout:
+        Login();
     }
 }
 
@@ -238,7 +522,7 @@ void ShowATMMainMenue()
     cout << "\t[4] Check Balance.\n";
     cout << "\t[5] Logout.\n";
     cout << "===========================================\n";
-    (enATMMenueOptions)ReadMainMenueOption();
+    PerfromMainATMMenueOption((enATMMenueOptions)ReadMainATMMenueOption());
   
 
 }
@@ -271,4 +555,11 @@ void Login()
     } while (LoginFaild);
 
     ShowATMMainMenue();
+}
+
+int main()
+{
+    Login();
+
+    return 0;
 }
