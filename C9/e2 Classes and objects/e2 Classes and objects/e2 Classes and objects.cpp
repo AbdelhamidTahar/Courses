369d031d1d9e1b1235d0386897a55fb229f60016
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-08-07 18:06:45 +0100

    add comments to understand what is data members

--- C9/e2 Classes and objects/e2 Classes and objects/e2 Classes and objects.cpp
+++ C9/e2 Classes and objects/e2 Classes and objects/e2 Classes and objects.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 using namespace std;
 
+// Any thing in class is Member.
+// We Have 2 Type of Members.
+//Data Member
+//Member Func(Method)
 class clsPerson
 {
+	//Data Member
 	bool IsLive;
 
 public:
 
+	//Data Member
 	short Age;
+	//Data Member
 	bool Sex;
+	//Data Member
 	string FirstName;
+	//Data Member
 	string LastName;
+	//Data Member
+
+	//Member Func
 	string FullName()
 	{
 		return FirstName + " " + LastName;
 	}
+	//Member Func
 	void PrintPersonInfo()
 	{
 		cout << "FullName: " << FullName() << endl;
@@ -35,7 +48,7 @@ int main()
 	Person1.Age = 24;
 	Person1.Sex = 1;
 	Person1.PrintPersonInfo();
-
+	
 
 	return 0;
 }
