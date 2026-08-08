11121290dfe439e08baf5fbc1b59d4fb16432248
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-08-08 16:06:20 +0100

    test Access Specifiers/Modifiers

--- C9/e2 Classes and objects/e2 Classes and objects/e2 Classes and objects.cpp
+++ C9/e2 Classes and objects/e2 Classes and objects/e2 Classes and objects.cpp
@@ -1,54 +1,54 @@
 #include <iostream>
 using namespace std;
 
-// Any thing in class is Member.
-// We Have 2 Type of Members.
-//Data Member
-//Member Func(Method)
 class clsPerson
 {
-	//Data Member
-	bool IsLive;
+private:
+
+	int Var1 = 65;
+	int Var2 = 66545;
+
+	int PrivateFunc1()
+	{ 
+		return 100;
+	}
+	int PrivateFunc2()
+	{
+		return 100;
+	}
+
+protected:
+
+	int PretectedFunc1()
+	{
+		return PrivateFunc2() + PrivateFunc1();
+	}
+	int PretectedFunc2()
+	{
+		return PrivateFunc1() + PrivateFunc2();
+	}
+
 
 public:
-
-	//Data Member
-	short Age;
-	//Data Member
-	bool Sex;
-	//Data Member
 	string FirstName;
-	//Data Member
 	string LastName;
-	//Data Member
-
-	//Member Func
-	string FullName()
+	string FullName;
+	short Age;
+	short VarPerson = PretectedFunc2();
+	string GetFullName()
 	{
 		return FirstName + " " + LastName;
 	}
-	//Member Func
-	void PrintPersonInfo()
-	{
-		cout << "FullName: " << FullName() << endl;
-		cout << "Age: " << Age << endl;
-		cout << "Sex: " << Sex << endl;
-		cout << "Is Live: " << IsLive << endl;
-	}
-
 };
 
 int main()
 {
-
 	clsPerson Person1;
 
 	Person1.FirstName = "Abdelhamid";
 	Person1.LastName = "TAHAR";
 	Person1.Age = 24;
-	Person1.Sex = 1;
-	Person1.PrintPersonInfo();
-	
+	Person1.FullName = Person1.GetFullName();
 
 	return 0;
 }
