b415e56b75b3dd219ebf5e776c0bc49d41646150
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-08-10 17:19:57 +0100

    alis get and get property

--- C9/Read Only Property/Read Only Property/Read Only Property.cpp
+++ C9/Read Only Property/Read Only Property/Read Only Property.cpp
@@ -16,30 +16,34 @@ private:
 
 public:
 	// ID is Read Only.
-	int ID()
+	int GetID()
 	{
 		return _ID;
 	}
+	__declspec(property(get = GetID))int ID;
+
 
 	void SetFirstName(string FirstName)
 	{
 		_FirstName = FirstName;
 	}
-	string FirstName()
+	string GetFirstName()
 	{
 		return _FirstName;
 	}
+	__declspec(property(get = GetFirstName, put = SetFirstName)) string FirstName;
 
 	void SetLastName(string LastName)
 	{
 		_LastName = LastName;
 	}
-	string LastName()
+	string GetLastName()
 	{
 		return _LastName;
 	}
+	__declspec(property(get = GetLastName, put = SetLastName)) string LastName;
 
-	string FullName()
+	string GetFullName()
 	{
 		return _FirstName + _LastName;
 	}
@@ -48,46 +52,50 @@ public:
 	{
 		_Age = Age;
 	}
-	short Age()
+	short GetAge()
 	{
 		return _Age;
 	}
+	__declspec(property(get = GetAge, put = SetAge)) short Age;
 
 	void SetSex(bool Sex)
 	{
 		_Sex = Sex;
 	}
-	bool Sex()
+	bool GetSex()
 	{
 		return _Sex;
 	}
+	__declspec(property(get = GetSex, put = SetSex))bool Sex;
 
 	void SetIsLive(bool IsLive)
 	{
 		_IsLive = IsLive;
 	}
-	bool IsLive()
+	bool GetIsLive()
 	{
 		return _IsLive;
 	}
+	__declspec(property(get = GetIsLive, put = SetIsLive)) bool IsLive;
 };
 
 int main()
 {
 	clsPerson Person1;
 
-	Person1.SetFirstName("Abdelhamid");
-	Person1.SetLastName("TAHAR");
-	Person1.SetAge(24);
-	Person1.SetSex(1);
-	Person1.SetIsLive(1);
+	Person1.FirstName = "Abdelhamid";
+	Person1.LastName = "TAHAR";
+	Person1.Age = 24;
+	Person1.Sex = 1;
+	Person1.IsLive = 1;
+	
 
-	cout << "ID: " << Person1.ID() << endl;
-	cout << "First Name: " << Person1.FirstName() << endl;
-	cout << "Last Name: " << Person1.LastName() << endl;
-	cout << "Age: " << Person1.Age() << endl;
-	cout << "Sex: " << Person1.Sex() << endl;
-	cout << "Is Live: " << Person1.IsLive() << endl;
+	cout << "ID: " << Person1.ID << endl;
+	cout << "First Name: " << Person1.FirstName << endl;
+	cout << "Last Name: " << Person1.LastName << endl;
+	cout << "Age: " << Person1.Age << endl;
+	cout << "Sex: " << Person1.Sex << endl;
+	cout << "Is Live: " << Person1.IsLive << endl;
 
 	return 0;
 }
