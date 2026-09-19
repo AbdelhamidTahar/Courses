641a87f225a1c977f41267a48f563f62f3d11705
AbdelhamidTahar <taharabdalhamid12@gmail.com>
2026-09-19 13:56:13 +0100

    creat dynamic array with objects

---
+++ C9/Dynamic array with objects/Dynamic array with objects/Dynamic array with objects.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+class clsPerson
+{
+private:
+	string _FirstName;
+	string _LastName;
+
+public:
+	void SetFirstName(string FirstName)
+	{
+		_FirstName = FirstName;
+	}
+	string FirstName()
+	{
+		return _FirstName;
+	}
+
+	void SetLastName(string LastName)
+	{
+		_LastName = LastName;
+	}
+	string LastName()
+	{
+		return _LastName;
+	}
+
+
+	clsPerson(string FirstName, string LastName)
+	{
+		_FirstName = FirstName;
+		_LastName = LastName;
+	}
+
+	clsPerson()
+	{
+
+	}
+
+};
+
+int main()
+{
+	int NumberOfObjects = 100;
+	clsPerson* ObjectsArray = new clsPerson[NumberOfObjects];
+
+
+	for (int i = 0; i < NumberOfObjects; i++)
+	{
+		ObjectsArray[i].SetFirstName("Test");
+		ObjectsArray[i].SetLastName("Tast");
+	}
+
+	for (int i = 0; i < NumberOfObjects; i++)
+	{
+		cout << ObjectsArray[i].FirstName()
+			<< ObjectsArray[i].LastName() << endl;
+	}
+
+	delete []ObjectsArray;
+	return 0;
+}
